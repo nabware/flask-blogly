583cf737513dda4e593271f82bf95d15ac3d9e53
Silu Hu <erinh816@gmail.com>
2023-10-05 17:01:26 -0400

    add post model

--- models.py
+++ models.py
@@ -34,3 +34,38 @@ class User(db.Model):
         db.Text,
         nullable=False,
         default=DEFAULT_IMAGE_URL)
+
+
+
+
+class Post(db.Model):
+    """Post"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    title = db.Column(
+        db.String(50),
+        db.CheckConstraint('LENGTH(title) >= 2'),
+        nullable=False)
+
+    content = db.Column(
+        db.Text,
+        nullable=False)
+
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow)
+        #time check
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id')
+        )
+
+    user = db.relationship('User', backref = 'posts')
